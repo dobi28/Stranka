0e5d9c54f322e318da12c248b7c1b12fec141b8d
strakaadrian <36277497+strakaadrian@users.noreply.github.com>
2018-10-29 21:00:20 +0100

    upravil so haha

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<?php
-mb_internal_encoding("UTF-8");
-
-// funkcia, ktorá mi podľa požiadavky sama naloaduje buď controller alebo model
-function autoLoadFunction($class){
-    //ked sa konci na controller loadne Controller
-    if(preg_match('/Controller$/', $class)){
-        require ("Controller/" . $class . ".php");
-    } else {
-        require("Model/" . $class . ".php");
-    }
-}
-//php ju bude vykonavat ako autoloader
-spl_autoload_register("autoloadFunction");
-
-$smerovac = new RouterController();
-$smerovac->process(array($_SERVER['REQUEST_URI']));
-$smerovac->showView();
+Ahoj som macka
